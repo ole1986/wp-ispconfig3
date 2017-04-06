b2e139e70515906a751c883ad3c4ebbbb5a90500
ole <ole.k@web.de>
2017-04-06 20:57:00 +0200

    purchasable is always true and price equals to regular price

--- wc/ispconfig_wc_product.php
+++ wc/ispconfig_wc_product.php
@@ -6,4 +6,37 @@ abstract class WC_ISPConfigProduct extends WC_Product {
     abstract public function OnCheckout($checkout);
     abstract public function OnCheckoutValidate();
     abstract public function OnCheckoutSubmit($order_id, $item_key, $item);
+
+    public function is_purchasable() {
+        return true;
+    }
+
+    public function get_sold_individually() {
+        return true;
+    }
+
+    public function get_price(){
+        return $this->get_regular_price();
+    }
+
+    /**
+     * Get the add to url used mainly in loops.
+     *
+     * @return string
+     */
+    public function add_to_cart_url() {
+        $url = $this->is_purchasable() && $this->is_in_stock() ? remove_query_arg( 'added-to-cart', add_query_arg( 'add-to-cart', $this->id ) ) : get_permalink( $this->id );
+        return $url;
+    }
+
+    /**
+     * Get the add to cart button text.
+     *
+     * @return string
+     */
+    public function add_to_cart_text() {
+        $text = $this->is_purchasable() && $this->is_in_stock() ? __( 'Add to cart', 'woocommerce' ) : __( 'Read more', 'woocommerce' );
+
+        return apply_filters( 'woocommerce_product_add_to_cart_text', $text, $this );
+    }
 }
