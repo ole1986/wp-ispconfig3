39e4a9d3d4e7269cf3d7db85d1501d01208cb376
ole <ole.k@web.de>
2019-10-30 16:11:19 +0100

    Install git pre-commit as symlink

--- install/GitPreCommit.php
+++ install/GitPreCommit.php
@@ -11,14 +11,12 @@ class GitPreCommit
     public static function install(Event $event)
     {
         echo "Installing pre-commit script for git\n";
-        $content = "#!/bin/sh\n";
-        $content.= "files=()\n";
-        $content.= 'for f in $(git diff --name-only --cached); do files+=($f); done' . "\n";
-        $content.= '[ ${#files[@]} -gt 0 ] && vendor/bin/phpcs ${files[@]}' . "\n";
 
-        file_put_contents(self::$preCommitFile, $content);
+        if (file_exists(self::$preCommitFile)) {
+            unlink(self::$preCommitFile);
+        }
 
-        echo "Make pre-commit executable\n";
-        chmod(self::$preCommitFile, 0755);
+        symlink('../../install/pre-commit', self::$preCommitFile);
+        chmod('install/pre-commit', 0755);
     }
 }
